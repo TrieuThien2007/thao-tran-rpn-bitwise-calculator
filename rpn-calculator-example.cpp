2a75fb47815fbe3dd10acec2014ecb99355feb90
TrieuThien2007 <nguyentrieuthien2906@gmail.com>
2026-09-15 15:55:07 -0700

    Implement Enter Command

--- rpn-calculator-example.cpp
+++ rpn-calculator-example.cpp
@@ -47,11 +47,16 @@ uint8_t const width = 16U;
  * Students should create or add any data structures needed.
  * Students should create or add any functions or classes they may need.
  */
+
+vector<uint16_t> rpn_stack;
+
 shared_ptr<uint16_t> rpn_calc(command const cmd, uint16_t const value = 0) {
-    // this is example code which returns a (smart shared) pointer to 16-bit value
-    uint16_t val = 0b1001100100000011;
-    shared_ptr<uint16_t> result = make_shared<uint16_t>(val);
-    return result;
+    if (cmd == cmd_enter) {
+        rpn_stack.push_back(value);
+        return make_shared<uint16_t>(rpn_stack.back());
+    }
+
+    return nullptr;
 }
 
 /*
